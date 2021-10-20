acba747a86d9d505776ab6cd9801818406b75fcb
Carlos Hernandez <carlosheragu2003@gmail.com>
2021-10-19 20:29:11 -0500

    Form Actualizado

--- webcommerce_actualizada/src/pages/admin/Usuarios.jsx
+++ webcommerce_actualizada/src/pages/admin/Usuarios.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import {Prueba} from './Prueba'
 import { FormularioMelo } from './Prueba';
 import FormUsuarios from '../../components/FormUsuarios'
+import FormUsuarios2 from '../../components/FormUsuario2'
 
 function Usuarios() {
   return(
     // <Prueba/>
     // <FormularioMelo/>
-    <FormUsuarios/>
+    // <FormUsuarios/>
+    <FormUsuarios2/>
   )
 }
 
