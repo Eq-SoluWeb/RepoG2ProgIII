52f79f75852978d14bbc09749d8c2530061f4a93
Carlos Hernandez <carlosheragu2003@gmail.com>
2021-10-26 11:05:36 -0500

    Se dejo la carpeta webcommerce2021

--- Funcional/src/pages/NotFound.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const NotFound=()=><h1>Pagina No encontrada</h1>
-export default NotFound;
--- webcommerce_actualizada/src/components/ImagenLogo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Logo from '../assets/img/logo.png';
-
-const ImagenLogo = () => {
-  return <img className='mx-auto h-40 w-auto' src={Logo} alt='Workflow' />;
-};
-
-export default ImagenLogo;
